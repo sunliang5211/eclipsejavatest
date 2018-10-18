78b512beef4b32e1c2c0b1382d013397cb0ff43e
sunliang5211 <sunliang5211@163.com>
2018-10-18 16:24:53 +0800

    增加 yangyitong

--- javatest/src/javatest/HelloJava.java
+++ javatest/src/javatest/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("hello java");
 		System.out.println("hello sunliang");
+		System.out.println("hello yanyitong");
 	}
 }
